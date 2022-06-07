ec65f5dcdabf33d7d5824f96772151bc25e29c26
santosandressa <andressasantosp0@gmail.com>
2022-06-07 13:24:25 -0300

    task 4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+nome varchar(80) NOT NULL,
+email varchar(40) NOT NULL,
+telefone int
+);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
