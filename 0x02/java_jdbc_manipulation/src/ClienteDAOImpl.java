1a876219bd7f57db00189b339bf80bd4375f2412
santosandressa <andressasantosp0@gmail.com>
2022-06-13 11:00:45 -0300

    task 2 - refactoring

--- 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
+++ 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
@@ -9,26 +9,30 @@ import java.util.logging.Logger;
 public class ClienteDAOImpl implements ClienteDAO {
 
     Logger logger = Logger.getLogger(ClienteDAOImpl.class.getName());
-    private Connection connection;
 
-    public ClienteDAOImpl() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            this.connection = DriverManager.getConnection("jdbc:sqlite:sqlite_database_marco_2022.db");
-            logger.info("Connection to SQLite has been established.");
-        } catch (Exception e) {
-            throw new RuntimeException("Connection to SQLite has not been established.", e);
-        }
-    }
+    private Connection connection;
 
     @Override
     public Connection connect(String urlConexao) {
-        return new ClienteDAOImpl().connection;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            urlConexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(urlConexao);
+            Class.forName("org.sqlite.JDBC");
+            logger.info("Connection to SQLite has been established.");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+        }
+        return this.connection;
     }
 
     @Override
     public void createTable(String urlConexao) {
         try {
+            Class.forName("org.sqlite.JDBC");
+            urlConexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(urlConexao);
             String sql = "CREATE TABLE IF NOT EXISTS Cliente (id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER, cpf TEXT, rg TEXT)";
             connection.createStatement().execute(sql);
             logger.info("Table Cliente has been created.");
@@ -40,6 +44,9 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void insert(String url_conexao, Cliente cliente) {
         try {
+            Class.forName("org.sqlite.JDBC");
+            url_conexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(url_conexao);
             String sql = String.format("INSERT INTO Cliente (nome, idade, cpf, rg) VALUES ('%s', %d, '%s', '%s')", cliente.getNome(), cliente.getIdade(), cliente.getCpf(), cliente.getRg());
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.executeUpdate();
@@ -52,6 +59,9 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void selectAll(String urlConexao) {
         try {
+            Class.forName("org.sqlite.JDBC");
+            urlConexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(urlConexao);
             String sql = String.format("SELECT * FROM Cliente");
             PreparedStatement statement = connection.prepareStatement(sql);
             ResultSet rs = statement.executeQuery();
@@ -73,6 +83,9 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void update(String urlConexao, int id, String name, Integer idade) {
         try {
+            Class.forName("org.sqlite.JDBC");
+            urlConexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(urlConexao);
             String sql = String.format("UPDATE Cliente SET nome = '%s', idade = %d WHERE id = %d", name, idade, id);
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.executeUpdate();
@@ -85,10 +98,13 @@ public class ClienteDAOImpl implements ClienteDAO {
     @Override
     public void delete(String urlConexao, int id) {
         try {
+            Class.forName("org.sqlite.JDBC");
+            urlConexao = "jdbc:sqlite:sqlite_database_marco_2022.db";
+            this.connection = DriverManager.getConnection(urlConexao);
             String sql = String.format("DELETE FROM Cliente WHERE id = %d", id);
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = this.connection.prepareStatement(sql);
             statement.executeUpdate();
-            logger.info("Cliente has been deleted.");
+            logger.info("Connection to SQLite has been established.");
         } catch (Exception e) {
             throw new RuntimeException("Cliente has not been deleted.", e);
         }
